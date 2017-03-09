d7d3aa5dc8a513b2deda321e1dc1972a5d5ad54a
chenyongh <chenyongh@sunaw.net>
2017-03-09 11:30:40 +0800

    2017年3月9日11:30:38

--- src/javascripts/BigOrder.js
+++ src/javascripts/BigOrder.js
@@ -27,28 +27,7 @@
        }(),
        language: (navigator.browserLanguage || navigator.language).toLowerCase()
    }
-   if (browser.versions.mobile) { //判断是否是移动设备打开。browser代码在下面
-       var ua = navigator.userAgent.toLowerCase(); //获取判断用的对象
-       if (ua.match(/MicroMessenger/i) == "micromessenger") {
-           //在微信中打开
-           // alert('你在微信中打开')
-       }
-       if (ua.match(/WeiBo/i) == "weibo") {
-           //在新浪微博客户端打开
-       }
-       if (ua.match(/QQ/i) == "qq") {
-           //在QQ空间打开
-       }
-       if (browser.versions.ios) {
-           //是否在IOS浏览器打开
-       }
-       if (browser.versions.android) {
-           //是否在安卓浏览器打开
-       }
-   } else {
-       //否则就是PC浏览器打开
-       alert('1232')
-   }
+
 
    window.onload = function() {
        //        var wh = window.screen.height;
